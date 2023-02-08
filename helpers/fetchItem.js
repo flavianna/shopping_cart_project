98e2d9d5e305519b562db840340ef5dde97b25ff
flavianna <flaviannabe@gmail.com>
2023-02-08 01:24:41 -0300

    feat: Implementa funcionalidade de adição de itens ao carrinho, bem como sua remoção ao serem clicados.

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
-};
+const fetchItem = async (id) => {
+    try {
+      const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+      const data = await response.json();
+      return data;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
+  };
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 // Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
-// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições! 
+// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
 
-// Fique a vontade para modificar o código já escrito e criar suas próprias funções!
-
-/**
- * Função responsável por criar e retornar o elemento de imagem do produto.
- * @param {string} imageSource - URL da imagem.
- * @returns {Element} Elemento de imagem do produto.
- */
+// Função responsável por criar e retornar o elemento de imagem do produto.
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,13 +9,7 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
-/**
- * Função responsável por criar e retornar qualquer elemento.
- * @param {string} element - Nome do elemento a ser criado.
- * @param {string} className - Classe do elemento.
- * @param {string} innerText - Texto do elemento.
- * @returns {Element} Elemento criado.
- */
+// Função responsável por criar e retornar qualquer elemento.
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -29,26 +17,23 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-/**
- * Função responsável por criar e retornar o elemento do produto.
- * @param {Object} product - Objeto do produto. 
- * @param {string} product.id - ID do produto.
- * @param {string} product.title - Título do produto.
- * @param {string} product.thumbnail - URL da imagem do produto.
- * @returns {Element} Elemento de produto.
- */
-const createProductItemElement = ({ id, title, thumbnail }) => {
+// Função responsável por criar e retornar o elemento do produto.
+const createProductItemElement = ({ id, title, thumbnail, price }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item_id', id));
-  section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createCustomElement('span', 'item__title', price));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 };
-// cria listagem de produtos
+
+// Função que cria listagem de produtos.
 const creatList = async () => {
   const fetch = await fetchProducts('computador');
   const father = document.getElementsByClassName('items')[0];
@@ -57,21 +42,11 @@ const creatList = async () => {
     father.appendChild(creatElements);
   });
 };
-/**
- * Função que recupera o ID do produto passado como parâmetro.
- * @param {Element} product - Elemento do produto.
- * @returns {string} ID do produto.
- */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
-/**
- * Função responsável por criar e retornar um item do carrinho.
- * @param {Object} product - Objeto do produto.
- * @param {string} product.id - ID do produto.
- * @param {string} product.title - Título do produto.
- * @param {string} product.price - Preço do produto.
- * @returns {Element} Elemento de um item do carrinho.
- */
+// Função que remove o produto do carrinho ao ser clicado.
+const cartItemClickListener = (e) => e.target.remove();
+
+// Função responsável por criar e retornar um item do carrinho.
 const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -80,6 +55,24 @@ const createCartItemElement = ({ id, title, price }) => {
   return li;
 };
 
-window.onload = async () => { 
-  creatList();
+// Função que adiciona um item ao carrinho (pesca pelo id).
+const addItemToCart = async (id) => {
+  const fetchItemId = await fetchItem(id);
+  const father = document.getElementsByClassName('cart__items')[0];
+  const child = createCartItemElement(fetchItemId);
+  father.appendChild(child);
+};
+
+// Função adiciona um event listener a cada botão "item__add" na página. 
+const bindAddToCartButton = () => {
+  document.querySelectorAll('.item__add').forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      const selectedProductId = e.target.parentNode.firstChild.innerText;
+      addItemToCart(selectedProductId);
+    }));
+};
+
+window.onload = async () => {
+  await creatList();
+  bindAddToCartButton();
 };
